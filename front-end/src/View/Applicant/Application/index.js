810254a9a91eaa1b213d53f22f847bb4925216fc
Matthew Fan <kungfumo@gmail.com>
2021-03-09 00:43:59 -0500

    added ant design and separate component folder

--- front-end/src/View/Applicant/Application/index.js
+++ front-end/src/View/Applicant/Application/index.js
@@ -1,6 +1,11 @@
+import { Button } from "antd"
+
 const Application = () => {
   return (
-    <div>this is the application page</div>
+    <div>
+      this is the application page hello
+      <Button type="primary">Button</Button>
+    </div>
   )
 }
 
