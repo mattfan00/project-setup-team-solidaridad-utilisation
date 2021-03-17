1c722a76c8a2144c28b9ada56b3c71a48797d48e
Xianhui Zhu <xz2715@nyu.edu>
2021-03-17 15:05:29 -0400

    non-styling landing page

--- front-end/src/View/Landing/index.js
+++ front-end/src/View/Landing/index.js
@@ -1,7 +1,51 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import './index.css'; 
+import 'antd/dist/antd.css'; 
+
+
+import { Divider } from 'antd'; 
+
 const Landing = () => {
   return (
-    <div>
-      this is the Copply landing page
+      <Body/> 
+  )
+}
+
+const Body = () => {
+  return(
+    <div id="body">
+      <Header />
+      <Party />
+    </div>
+  )
+}
+
+//header: company logo and stuff
+const Header = () => {
+  return(
+    <div className="header">
+      <Divider orientation="right">
+        <Link to="/login">login</Link>
+      </Divider>
+      <span className="header-title">
+        Copply
+      </span>
+      <span className="header-text">
+        Making the application process seamless for both parties
+      </span>
+    </div>
+  )
+}
+
+const Party = () => {
+  return(
+    <div className="Party">
+      <span className="party-text">
+        Are you an applicant or company? 
+      </span>
+      <link to="/applicant">Applicant</link>
+      <link to="/signup">Company</link>
     </div>
   )
 }
