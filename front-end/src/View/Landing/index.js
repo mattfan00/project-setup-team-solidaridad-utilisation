4102dc086c0451f84fbbbac18eaba1f3ec916dd2
Xianhui Zhu <xz2715@nyu.edu>
2021-03-18 11:06:49 -0400

    positioned landing page

--- front-end/src/View/Landing/index.js
+++ front-end/src/View/Landing/index.js
@@ -4,50 +4,24 @@ import './index.css';
 import 'antd/dist/antd.css'; 
 
 
-import { Divider } from 'antd'; 
+import { Layout } from 'antd'; 
 import { Link } from "react-router-dom"; 
-
+import { Row, Col } from 'antd'; 
+import { Button } from 'antd'; 
 const Landing = () => {
   return (
-      <Body/> 
-  )
-}
-
-const Body = () => {
-  return(
-    <div id="body">
-      <Header />
-      <Party />
-    </div>
-  )
-}
-
-//header: company logo and stuff
-const Header = () => {
-  return(
-    <div className="header">
-      <Divider orientation="right">
-        <Link to="/login">login</Link>
-      </Divider>
-      <span className="header-title">
-        Copply
-      </span>
-      <span className="header-text">
-        Making the application process seamless for both parties
-      </span>
-    </div>
-  )
-}
-
-const Party = () => {
-  return(
-    <div className="Party">
-      <span className="party-text">
-        Are you an applicant or company? 
-      </span>
-      <Link to="/applicant">Applicant</Link>
-      <Link to="/signup">Company</Link>
-    </div>
+    <body>
+      <div class="main">
+          <h1>Copply</h1>
+          <h2>Making the application process seamless for both parties</h2>
+        <p>Are you an applicant or Company?</p> 
+        <div class="button">
+          <Button size="large" href="/applicant">Applicant</Button>
+          <Button size="large" href="/signup">Company</Button>
+        </div>
+      </div>
+    </body>
+      
   )
 }
 
