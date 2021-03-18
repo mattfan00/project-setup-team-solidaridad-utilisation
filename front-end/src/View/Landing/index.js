15740238cc86c610e34dd731d617ccfc5e4bfae7
Xianhui Zhu <xz2715@nyu.edu>
2021-03-18 14:17:51 -0400

    fixing route path name

--- front-end/src/View/Landing/index.js
+++ front-end/src/View/Landing/index.js
@@ -1,13 +1,9 @@
 import React from 'react'; 
-import ReactDOM from 'react-dom'; 
 import './index.css'; 
 import 'antd/dist/antd.css'; 
 
-
-import { Layout } from 'antd'; 
-import { Link } from "react-router-dom"; 
-import { Row, Col } from 'antd'; 
 import { Button } from 'antd'; 
+
 const Landing = () => {
   return (
     <body>
