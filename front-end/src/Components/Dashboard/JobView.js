f30eb1e78bb42b858731570a7e94e5b446f35bec
ando-huang <ahuang09182000@gmail.com>
2021-03-18 18:39:03 -0400

    created dropdown menu for the applicant listing

--- front-end/src/Components/Dashboard/JobView.js
+++ front-end/src/Components/Dashboard/JobView.js
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useHistory, Link } from "react-router-dom"
-import {Card} from "antd"
+import {Card, Dropdown, Menu} from "antd"
+import './styles.css'
+import {DownOutlined} from '@ant-design/icons'
 
 const JobView = (props) => {
+
+    const [status, setStatus] = useState('Open')
+
+    function handleMenuClick(i) {
+        console.log(i.key)
+        if(i.key == '1'){
+            setStatus('Open')
+        } 
+        else if(i.key == '2'){
+            setStatus('Closed')
+        }
+        else{
+            setStatus('Archived')
+        }
+    }
+
+    const menu = () =>{
+        return(
+            <Menu 
+                onClick={handleMenuClick}
+            >
+                <Menu.Item key='1'>Open</Menu.Item>
+                <Menu.Item key='2'>Close</Menu.Item>
+                <Menu.Item key='3'>Archive</Menu.Item>
+            </Menu>
+        );
+    }
 
     return (  
         <Card
             title = 'Applicants to $jobtitle$'
             extra = {
-                <div className = "counter">
-                    112{props.appCount}
+                <div className="headerextra">
+                    <div className="headCounter">
+                        Applications: 11{props.appCount}
+                    </div>
+                    <Dropdown
+                        overlay={menu()}
+                    >
+                        <a>
+                            {status} <DownOutlined/>
+                        </a>
+                    </Dropdown>                    
                 </div>
             }
         >
