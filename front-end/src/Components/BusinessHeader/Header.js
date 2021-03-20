d5f25383266bcb9a1ae598a207bcae35ade18d12
ando-huang <ahuang09182000@gmail.com>
2021-03-20 17:28:14 -0400

    updated to add the logo

--- front-end/src/Components/BusinessHeader/Header.js
+++ front-end/src/Components/BusinessHeader/Header.js
@@ -2,16 +2,21 @@ import React from 'react'
 import './styles.css'
 import Profile from './ProfileDropdown'
 import logo from '../ApplicantHeader/copply.png'
+import {PageHeader} from 'antd'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div className='welcomeText'>
-                $Company Name$
-            </div>
-            <div className="profile">
-                <Profile/>
-            </div>
+        <div className='top'>
+            <PageHeader
+                className="site-page-header"
+                title={
+                    <img src={logo} alt="website logo" className="logo" />
+                }
+                subTitle='Company name'
+                extra={
+                    <Profile />
+                }
+            />
         </div>
     )
 }
