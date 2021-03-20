5537af2c3b8649cd5d900758d6c63bd5850aa9de
jayliquek <jayliquek@gmail.com>
2021-03-19 20:50:42 -0400

    signin page

--- front-end/src/Components/ApplicantSignIn/index.js
+++ front-end/src/Components/ApplicantSignIn/index.js
@@ -1,17 +1,66 @@
 import React from "react"
-
-import {Card,Col, Button, Form, Input } from 'antd';
+import './index.css'
+import { Card, Col, Button, Form, Input,Checkbox } from 'antd';
+import {
+    Link,
+  } from "react-router-dom";
 
 const ApplicantSignIn = () => {
+    const layout = {
+        labelCol: {
+            span: 24,
+        },
+        wrapperCol: {
+            span: 24,
+        },
+    };
     return (
-        <div>
+        <div className="Applicant-SignIn-Card">
             <Col span={24}>
                 <Card>
-                    <p>Express Apply</p>
-                    <p>Already have an account?</p>
-                    
+                    <div className="Applicant-SignIn-Words">
+                        <h3>Express Apply</h3>
+                        <p className="NoAccount">Don't have an account? <Link to="/application/signup">Sign up</Link></p>
+                    </div>
+                        <Form
+                            {...layout}
+                            name="basic"
+                            initialValues={{
+                                remember: true,
+                            }}                          
+                        >
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your username!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item {...layout} name="remember" valuePropName="checked">
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                        </Form> 
+
+
                     <Button type="primary" block>
-                        Sign In 
+                        Continue with Copply
                     </Button>
                 </Card>
             </Col>
