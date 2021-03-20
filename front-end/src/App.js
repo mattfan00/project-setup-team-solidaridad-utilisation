b83303566b9172a6a96c45ba3c49cd54b747927a
Xianhui Zhu <xz2715@nyu.edu>
2021-03-20 19:13:58 -0400

    positioned editprofile page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,6 +15,7 @@ import BusinessApplicant from "./View/Business/Applicant"
 import BusinessSignin from "./View/Business/Signin"
 import NewListing from "./View/Business/NewListing"
 import DescriptionForm from "./View/Business/Description"
+import BusinessEditProfile from "./View/Business/EditProfile"
 
 import Application from "./View/Applicant/Application"
 import ApplicationSuccess from "./View/Applicant/Success"
@@ -40,6 +41,7 @@ const App = () => {
           <Route path="/business/listing/new" component={BusinessNewListing} />
           <Route path="/business/newlisting/description" component={DescriptionForm} />
           <Route path="/business/newlisting/elements" component={NewListing} />
+          <Route path="/business/dashboard/editprofile" component={BusinessEditProfile} />
 
           {/* All Applicant routes  */}
           <AuthProvider>
