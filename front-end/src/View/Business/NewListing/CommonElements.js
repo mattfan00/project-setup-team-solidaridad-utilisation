5a95295f16dc9419ec5916a650a1310fa5cf623d
Jin Sim <smjin55@gmail.com>
2021-03-21 18:48:26 +0900

    Added headers to business job description & common elements. Changed styling.

--- front-end/src/View/Business/NewListing/CommonElements.js
+++ front-end/src/View/Business/NewListing/CommonElements.js
@@ -11,8 +11,8 @@ const CommonElements = () => {
     };
     return (
         <div>
-        <Space direction="vertical" style={{ width:"50%" }}>
-            <Card style={{ borderColor:"#DADCFB", backgroundColor:"#DADCFB" }}>
+        <Space direction="vertical" style={{ width:"90%" }}>
+            <Card style={{ borderColor:"#DADCFB", "margin-bottom":"5%" }}>
                 <div className="elements-instruction">Tick all the details you want to require:</div>
                 <br></br>
                 <Form
