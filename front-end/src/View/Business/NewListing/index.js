36ba76005a1e6d35c75884c9d31f9bfaccc650f4
Jonathan Ko <jik290@nyu.edu>
2021-03-21 19:34:37 -0400

    fixing branch

--- front-end/src/View/Business/NewListing/index.js
+++ front-end/src/View/Business/NewListing/index.js
@@ -1,15 +1,55 @@
-
-import React from "react"
-import CommonElements from "./CommonElements"
+import React, { useState } from "react"
 import Header from '../../../Components/BusinessHeader/Header'
+import { Steps } from "antd"
+import JobDescription from "../Description/index"
+import CommonElements from "./CommonElements"
+import ExtraQuestions from "./ExtraQuestions/ExtraQuestions"
+import Preview from "./Preview/Preview"
+
+const { Step } = Steps;
 
 const NewListing = () => {
+
+
+
+  const [current, setCurrent] = useState(0);
+
+  const handleNextButton = () => {
+    setCurrent(current + 1);
+  }
+
+  // const handleBackButton = () => {
+  //   setCurrent(current - 1);
+  // }
+
+  
+  const steps = [
+    {
+      title: 'Job Description',
+      content: (<JobDescription handleNextButton={handleNextButton}/>)
+    },
+    {
+      title: 'Common Elements',
+      content: <CommonElements />
+    },
+    {
+      title: 'Add Extra Questions',
+      content: <ExtraQuestions />
+    },
+    {
+      title: 'Preview',
+      content: <Preview />
+    }
+  ]
   return (
-    <div className="checkBoxForm">
+    <div>
       <Header></Header>
-      {/* Insert Progress Bar Component */}
-      <h3>Insert Progress-Tracker (Common Elements)</h3>
-      <CommonElements></CommonElements>
+      <Steps current={current}>
+        {steps.map(item => (
+          <Step key={item.title} title = {item.title}/>
+        ))}
+      </Steps>
+      <div> {steps[current].content}</div>
     </div>
   )
 }
