7ecfc84fe30da48f4b1f93967ec5cfa9403e9d60
ando-huang <ahuang09182000@gmail.com>
2021-03-27 09:11:49 -0400

    route with json from mockapi

---
+++ back-end/routes/businessRoutes/getApplicationDetails.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const router = express.Router()
+
+// Get the applicant that is relevant to this application
+router.get("/application/details", (req, res) => {
+  res.json({
+        firstname: "Ricardo",
+        lastname: "Bogisich",
+        education: "New York University",
+        gradYear: "2022",
+        edDescription: "CS major",
+        work: [
+          {
+            id: "1",
+            company: "Kiehn - Effertz",
+            role: "Senior Integration Representative",
+            description: "Did stuff Global",
+            year: "2019"
+          },
+          {
+            company: "Yahoo!",
+            year: "2016",
+            role: "CEO",
+            description: "met with shareholders to discuss company goals"
+          }
+        ],
+        projects: [
+          {
+            id: "1",
+            title: "bandwidth",
+            description: "Corporate",
+            year: "2020"
+          }
+        ],
+        responses: [
+          {
+            commonQ: [
+              {
+                question: "Email",
+                answer: "apple@oranges.com"
+              },
+              {
+                question: "Veteran",
+                answer: "No"
+              }
+            ],
+            extraQ: [
+              {
+                question: "Which bear is best?",
+                answer: "black bear"
+              },
+              {
+                question: "Why are you interested in working with us?",
+                answer: "banana"
+              }
+            ]
+          }
+        ]
+      })
+    })
+
+module.exports = router
