b0a0356ff3db0bb38e392b35b15e3690516a35ec
jayliquek <jayliquek@gmail.com>
2021-04-01 23:11:42 -0400

    new new

--- back-end/app.js
+++ back-end/app.js
@@ -15,21 +15,15 @@ app.use(express.urlencoded({ extended: true }))
 // A section to import all of the routes in ./routes
 const getApplicantUser = require("./routes/getApplicantUser")
 const getApplicationDetails = require("./routes/businessRoutes/getApplicationDetails")
-<<<<<<< HEAD
 const getCompanyDetails = require("./routes/businessRoutes/getCompanyDetails")
-=======
 const getImageFromFile = require("./routes/getImageFromFile")
->>>>>>> 951b86f40c33619a3de9f71e7ead935966395200
 
 // This uses one of the imported routes
 app.use(getApplicantUser)
 app.use(getApplicationDetails)
-<<<<<<< HEAD
 app.use(getCompanyDetails)
-=======
 app.use(getImageFromFile)
 
->>>>>>> 951b86f40c33619a3de9f71e7ead935966395200
 
 app.get("/", (req, res) => {
   res.json({
