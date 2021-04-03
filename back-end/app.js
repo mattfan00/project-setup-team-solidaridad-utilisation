f388950902e9564189a03effe9aa958aa47b6184
Xianhui Zhu <xz2715@nyu.edu>
2021-04-03 13:48:47 -0400

    update

--- back-end/app.js
+++ back-end/app.js
@@ -18,11 +18,8 @@ const getBusinessJobs = require("./routes/getBusinessJobs")
 const getJobDetails = require("./routes/businessRoutes/getJobDetails")
 const getApplicationDetails = require("./routes/businessRoutes/getApplicationDetails")
 const getImageFromFile = require("./routes/getImageFromFile")
-<<<<<<< HEAD
 const postApplicationDetails = require("./routes/postApplicationDetails")
-=======
 // const changeJobStatus = require("./routes/businessRoutes/changeJobStatus")
->>>>>>> 5def48326fe9df600969a13a0b0b75501e22e7c5
 
 // This uses one of the imported routes
 app.use(getApplicantUser)
@@ -30,12 +27,9 @@ app.use(getJobDetails)
 app.use(getApplicationDetails)
 app.use(getImageFromFile)
 app.use(getBusinessJobs)
-<<<<<<< HEAD
 app.use(postApplicationDetails)
 
-=======
 // app.use(changeJobStatus)
->>>>>>> 5def48326fe9df600969a13a0b0b75501e22e7c5
 
 
 app.get("/", (req, res) => {
