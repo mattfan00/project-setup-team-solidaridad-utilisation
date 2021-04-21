eb7af2af75e1b8571eba2905626cbe858bed176d
jayliquek <jayliquek@gmail.com>
2021-04-21 00:04:59 -0400

    commit

--- back-end/routes/postBusinessProfile.js
+++ back-end/routes/postBusinessProfile.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const multer = require("multer")
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
 
+aws.config.update({
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  region: process.env.AWS_BUCKET_REGION
+});
+
+const s3 = new aws.S3();
 /*
 // enable logo uploads saved to disk in a directory named 'public/resumes'
 const storage = multer.diskStorage({
@@ -35,13 +44,27 @@ router.post("/logo-upload", upload.array("logo", 1), (req, res, next) => {
   })
 */
 
+const upload = multer({
+  storage: multerS3({
+      s3: s3,
+      bucket: process.env.AWS_BUCKET_NAME,
+      key: function (req, file, cb) {
+          console.log(file);
+          cb(null, file.originalname); //use Date.now() for unique file keys
+      }
+  })
+});
+
 //simulate the database for now
 const companies = []
 
-router.post("/updateprofile",(req, res)=>{
-    console.log(req.body.businessProfile)
-    companies.push(req.body.businessProfile)
-    res.json(req.body.businessProfile); 
+router.post("/updateprofile", upload.single("logo"), (req, res)=>{
+  companies.push({
+    description: req.body.description,
+    industry: req.body.industry,
+    logo: req.file.location,
+  })
+  res.json("Successfully updated"); 
 })
 
 
--- back-end/server.js
+++ back-end/server.js
@@ -1,34 +1,6 @@
 const express = require('express')
 const server = require("./app")
 
-const fs = require('fs')
-const utils = require('util')
-const unlinkFile = util.promisify(fs.unlink)
-
-const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
-
-const { uploadFile, getFileStream } = require('./s3')
-
-const app = express() 
-
-app.get('/images/:key', (req, res) => {
-  const key = req.params.key
-  const readStream = getFileStream(key)
-
-  readStream.pipe(res)
-})
-
-app.post('/images', upload.single('image'), (req, res) => {
-  const file = req.file
-  console.log(file)
-  const result = await uploadFile(file)
-  await unlinkFile(file.path)
-  console.log(result)
-  const description = req.body.description
-  res.send({imagePath: `/images/${result.Key}`})
-})
-
 const port = 4000
 
 // call a function to start listening to the port
