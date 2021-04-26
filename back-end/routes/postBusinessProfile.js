842d4fab5e0a763ac1beb5003bbc02a063f423ff
jayliquek <jayliquek@gmail.com>
2021-04-25 22:46:10 -0400

    aws linking

--- back-end/routes/postBusinessProfile.js
+++ back-end/routes/postBusinessProfile.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const router = express.Router()
 const multer = require("multer")
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
 const business = require("../models/business")
 const Business = require('../models/business')
 
+aws.config.update({
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  region: process.env.AWS_BUCKET_REGION
+});
+
+const s3 = new aws.S3();
 /*
 // enable logo uploads saved to disk in a directory named 'public/resumes'
 const storage = multer.diskStorage({
@@ -37,6 +46,27 @@ router.post("/logo-upload", upload.array("logo", 1), (req, res, next) => {
   })
 */
 
+const upload = multer({
+  storage: multerS3({
+      s3: s3,
+      bucket: process.env.AWS_BUCKET_NAME,
+      key: function (req, file, cb) {
+          console.log(file);
+          cb(null, file.originalname); //use Date.now() for unique file keys
+      }
+  })
+});
+
+//simulate the database for now
+const companies = []
+
+router.post("/updateprofile", upload.single("logo"), (req, res)=>{
+  companies.push({
+    description: req.body.description,
+    industry: req.body.industry,
+    logo: req.file.location,
+  })
+  res.json("Successfully updated"); 
 //get one
 router.get("/updateprofle/:id", getBusiness, (req, res) => {
     res.send(res.business)
@@ -63,11 +93,11 @@ router.post("/updateprofile", async (req, res)=>{
 //update one
 router.patch('/:id', getBusiness, async (req, res) => {
   if(req.body.businessProfile.description != null){
-    res.business.description = req.body.businessProfile.description
+    res.business.description = req.body.description
 
   }
   if(req.body.businessProfile.introduction != null){
-    res.business.introduction = req.body.businessProfile.introduction
+    res.business.introduction = req.body.industry
   }
   try{
     const updatedBusiness = await res.business.save()
