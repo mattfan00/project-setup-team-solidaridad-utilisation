6b2767431d458ce80866b63ae2528f45c39fce06
Matthew Fan <kungfumo@gmail.com>
2021-05-01 00:31:45 -0400

    retrive company by id in header

--- front-end/src/Components/BusinessHeader/Header.js
+++ front-end/src/Components/BusinessHeader/Header.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 import Profile from './ProfileDropdown'
 import logo from '../ApplicantHeader/copply.png'
 import { PageHeader } from 'antd'
 import { useHistory, Link } from 'react-router-dom'
+import axios from "axios"
 
 const Header = (props) => {
     const history = useHistory()
+
+    const [company, setCompany] = useState()
+
+    useEffect(async () => {
+        const result = await axios.get(`/company/${props.company}`)
+        setCompany(result.data)
+    }, [])
 
     return (
         <div className='top'>
