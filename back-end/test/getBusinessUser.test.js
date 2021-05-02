09369157eb79caa8e5c47a0292b78cde6113f7e8
Xianhui Zhu <xz2715@nyu.edu>
2021-05-01 22:28:27 -0400

    getBusinessUser works now

--- back-end/test/getBusinessUser.test.js
+++ back-end/test/getBusinessUser.test.js
@@ -5,11 +5,23 @@ chai.use(chaiHttp)
 
 const app = require("../app")
 
+const testUser = {
+  email: 'test@test.com', 
+  password: 'hello'
+}
+
+
 describe("getBusinessUser" , () => {
   it("should return status 200", async () => {
     let res = await chai
+    .request(app)
+    .post('/business/user/login')
+    .send(testUser)
+
+    res = await chai
       .request(app)
       .get("/business/user")
+      .set("Business-Authorization", `Bearer ${res.body.token}`)
       .send()
 
     expect(res.status).to.equal(200)
